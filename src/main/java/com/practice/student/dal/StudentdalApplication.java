e801fec663afcccaa7744ccd991e84dd7690287a
Pramod Kumar <36308910+Pramodk16@users.noreply.github.com>
2020-12-09 11:47:50 +0530

    Update StudentdalApplication.java

--- src/main/java/com/practice/student/dal/StudentdalApplication.java
+++ src/main/java/com/practice/student/dal/StudentdalApplication.java
@@ -8,7 +8,7 @@ public class StudentdalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentdalApplication.class, args);
-		System.out.println("Application is Running...");
+		System.out.println("Application is Up and Running...");
 	}
 
 }
